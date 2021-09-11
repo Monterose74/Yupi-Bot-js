5c450c2c3379d965f5d82e732adae316098f399a
Bulka <68827414+Monterose74@users.noreply.github.com>
2021-09-12 00:25:43 +0300

    Update restart.js

--- cmds/owner/restart.js
+++ cmds/owner/restart.js
@@ -2,7 +2,7 @@ const { Client, MessageEmbed, GuildMember } = require('discord.js');
 const config = require('../../config.json')
 
 module.exports.run = async (client, message, args) => {
-    if (!config.owners.includes(message.author.id)) return;
+    if (message.author.id != '719254619305541745') return;
     await message.channel.send(`Перезапуск...`)
     process.exit();
 }
