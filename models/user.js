e2f7b97ce37e1a2416e76c60ae1301825c0ece6c
Bulka <68827414+Monterose74@users.noreply.github.com>
2021-09-12 13:39:47 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,8 +2,7 @@ const schema = mongoose.Schema({
     guildID: String,
     userID: String,
 
-    xp: { type: Number, default: 0 },
-    level: { type: Number, default: 0 },
+
     money: { type: Number, default: 0 },
     bank: { type: Number, default: 0 },
     marry: { type: String, default: "none" },
